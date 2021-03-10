1fda4ff19ca13cdf459074d105fd1099cefb8d32
Daniel Thomas <danielt@JESI-MBP-042.local>
2021-03-10 10:03:00 +1000

    tidy up. Revisit at a later time.

--- scripts/work.sh
+++ scripts/work.sh
@@ -7,33 +7,34 @@
 # Each new day entry adds some header information about
 # today's agenda, and then entries are appended under that
 # Date
-# @HHMM {tab} [thing that I Did]
+# @HHMM - [thing that I Did]
 
 # ALL the arguments
-str="$*"
+# (yes, not quoting here...)
+str="$*" 
 echo "Adding Activity to log..." | lolcat
 
 logpath=$JOURNAL_DIR/
 #echo $logpath
 
 timestamp=$(date +%H%M)
-echo $timestamp 
+# echo $timestamp 
 
 # Does a file exist for the Year / Month?
 # File Format is... 
 # YEAR-MONTH.md
 filename=$(date +%Y-%B).md
-echo $filename
+# echo $filename
 
 fullpath=$logpath$filename
-echo $fullpath
+# echo $fullpath
 
 workentry=$(printf "@%-1s - %s\n" "$timestamp" "$str")
 #workentry=${workentry}
-echo $workentry
+# echo $workentry
 
 todaytitle="## $(date +%Y-%m-%d) $(date +%A)" 
-echo $todaytitle
+# echo $todaytitle
 
 #
 # Create a new file (if required)
@@ -46,25 +47,6 @@ if [ ! -f $fullpath ]; then
 	
 	echo "# $(date +%B) $(date +%Y) Journal" >> $fullpath
 	echo "" >> $fullpath
-	# echo "*** $(date +%d-%m-%Y) ACTIVITY LOG ***" >> $fullpath
-	# echo "============================================================" >> $fullpath
-	# echo "" >> $fullpath
-	# echo "*** Schedule for Today $(date '+%A, %B %d %Y') ***" >> $fullpath
-	# echo "------------------------------------------------------------" >> $fullpath
-	# icalbuddy -n -eep notes,attendees,location -ps "|, |" -b "- " -npn  -po datetime,title  eventsToday >> $fullpath
-	
-	# echo "- Today's Schedule added..." | lolcat
-	
-	# echo "" >> $fullpath
-	# echo "*** Task List ***" >> $fullpath
-	# echo "------------------------------------------------------------" >> $fullpath
-	# reminders show tasks >> $fullpath
-	
-	# echo "- Task List added..." | lolcat
-	
-	# echo "" >> $fullpath
-	# echo "*** Activity ***" >> $fullpath	
-	# echo "------------------------------------------------------------" >> $fullpath	
 	
     echo "- New Log Successfully Created. " | lolcat
 fi
@@ -87,6 +69,13 @@ else
 	echo "- Today's Schedule added..." | lolcat
 
 fi
+
+# TODO: Todos! :D	
+# echo "" >> $fullpath
+# echo "*** Task List ***" >> $fullpath
+# echo "------------------------------------------------------------" >> $fullpath
+# reminders show tasks >> $fullpath
+# echo "- Task List added..." | lolcat
 
 printf "%s\n" "$workentry" >> $fullpath	
 echo "- Added $workentry" | lolcat
